f2341966e49a8d664ccd4b35fdf955b6136bb94b
BlackBirdTV <chronicalcoding@gmail.com>
2023-06-29 17:49:29 +0200

    Set fixed Window Start size because of problem on KDE

--- ObjectiveLearn/MainForm.cs
+++ ObjectiveLearn/MainForm.cs
@@ -2,7 +2,6 @@ using Eto.Drawing;
 using Eto.Forms;
 using ObjectiveLearn.Components;
 using System;
-using System.IO;
 using TankLite;
 
 namespace ObjectiveLearn
@@ -11,43 +10,36 @@ namespace ObjectiveLearn
 	{
         public static TankLiteRuntimeEnvironment TLEnv { get; set; }
 
-        private TopBar TopBar { get; set; }
-        private Canvas Canvas { get; set; }
-        private ConsoleBar Console { get; set; }
-        private SideBar SideBar { get; set; }
-
         public MainForm(string title = "Objective: Learn")
 		{
 			Title = title;
 			MinimumSize = new Size(200, 200);
-			Size = new Size(
-				(int)Screen.Bounds.Width - 200,
-				(int)Screen.Bounds.Height - 200);
+			Size = new Size(800, 500);
 
 			Location = new Point(100, 100);
 
             BackgroundColor = ConfigurationManager.GetColor(Config.WindowColor);
 
-            TopBar = new();
-            Canvas = new()
+            var topBar = new TopBar();
+            var canvas = new Canvas()
             {
                 Location = new(0, 0)
             };
-            SideBar = new();
-            Console = new();
+            var sideBar = new SideBar();
+            var console = new ConsoleBar();
 
-            Console.UpdateShapes += (s, e) => Canvas.UpdateShapes();
-            Canvas.SelectShape += (s, e) => SideBar.SelectObject(e);
+            console.UpdateShapes += (s, e) => canvas.UpdateShapes();
+            canvas.SelectShape += (s, e) => sideBar.SelectObject(e);
 
             var layout = new DynamicLayout();
 
             layout.BeginHorizontal(true);
             layout.BeginVertical(null, null, true, true);
-            layout.Add(TopBar, true, false);
-            layout.Add(Canvas, true, true);
-            layout.Add(Console, true, false);
+            layout.Add(topBar, true, false);
+            layout.Add(canvas, true, true);
+            layout.Add(console, true, false);
             layout.EndBeginVertical();
-            layout.Add(SideBar, false, true);
+            layout.Add(sideBar, false, true);
             layout.EndHorizontal();
 
             Content = layout;
